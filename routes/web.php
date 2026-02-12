5b405265f2453394d3a4f54da8279e3733f1390b
Brandon Fields <superboomda@gmail.com>
2026-02-11 20:57:07 -0500

    undoing web route changes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index');
+    return view('welcome');
 });
 
-
+Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index');
