2ed0ea1cabd0f19259c6a92c73aede4b2c385860
Brandon Fields <superboomda@gmail.com>
2026-02-11 21:33:28 -0500

    Changed the way the fallout randomizer js works just slightly

--- public/js/fallout-special-randomizer.js
+++ public/js/fallout-special-randomizer.js
@@ -1,14 +1,12 @@
 //Script for Brandon's personal project:
-var strength = 1, perception = 1, endurance = 1, charisma = 1, intellegence = 1, agility = 1, luck = 1; //Declare each stat as a variable, each initialized to equal 1
-
 var limit = 28; //total number of starting points for S.P.E.C.I.A.L. stats
-
 var max = 21; //max amount of available points to distribute
-
 var min = 1; //min amount of available points to distribute
 
 //Calculate function to randomize each value, then check to make sure all values add to 28.
 function calculate() {
+  var strength = 1, perception = 1, endurance = 1, charisma = 1, intellegence = 1, agility = 1, luck = 1; //Declare each stat as a variable, each initialized to equal 1
+
   while (strength + perception + endurance + charisma + intellegence + agility + luck !== limit) {
     //get a randomized whole number between 1 (min) and 21 (max)
     strength = Math.floor(Math.random() * (+max - +min) + +min);
@@ -43,5 +41,13 @@ function calculate() {
 }
 
 function reset() {
-  location.reload();
+  var strength = 1, perception = 1, endurance = 1, charisma = 1, intellegence = 1, agility = 1, luck = 1;
+
+  document.getElementById('strength-output').innerHTML = strength;
+  document.getElementById('perception-output').innerHTML = perception;
+  document.getElementById('endurance-output').innerHTML = endurance;
+  document.getElementById('charisma-output').innerHTML =  charisma;
+  document.getElementById('intellegence-output').innerHTML =  intellegence;
+  document.getElementById('agility-output').innerHTML =  agility;
+  document.getElementById('luck-output').innerHTML =  luck;
 }
