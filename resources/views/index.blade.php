1bb007ddf7351dd143b1ad67dd0e11b35521d5cd
Brandon Fields <superboomda@gmail.com>
2026-02-11 23:50:23 -0500

    Updated margins for main div in content on both index and about.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-    <div class='rvt-flow rvt-prose'>
+    <div class='rvt-flow rvt-prose rvt-m-top-lg rvt-m-left-lg'>
         <h2>Welcome to diamo12's personal website!</h2>
         <p>Hello world! This website is a self-hosted project, with the goal of hosting all of my various personal web app projects for the fun of it. You can find more information about me, or any of the apps on this site on the <a href="{{route('about')}}">about</a> page.</p>
     </div>
