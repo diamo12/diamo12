0a45497f184db2fe4e0b9cf0b8a8432be3d11439
Brandon Fields <superboomda@gmail.com>
2026-02-11 20:56:20 -0500

    Testing updated web route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index');
 });
 
-Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index');
+
