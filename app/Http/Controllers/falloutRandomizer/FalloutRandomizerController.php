32388a6050ee86611240bd12efc992625a6f27ef
Brandon Fields <superboomda@gmail.com>
2026-02-11 21:02:19 -0500

    Updated fallout randomizer controller to use illuminate request module

--- app/Http/Controllers/falloutRandomizer/FalloutRandomizerController.php
+++ app/Http/Controllers/falloutRandomizer/FalloutRandomizerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\falloutRandomizer;
 
 use App\Http\Controllers\Controller;
 
+use Illuminate\Http\Request;
+
 class FalloutRandomizerController extends Controller
 {
     public function index(Request $request) {
