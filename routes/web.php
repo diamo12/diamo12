72d05932e8779e93d609337c3712b5867d010d68
Brandon Fields <superboomda@gmail.com>
2026-02-11 22:56:08 -0500

    Updated web routes and layout

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index')->name('falloutRandomizer');
-Route::get('/tip-calc', 'App\Http\Controllers\tipCalc\tipCalcController@index')->name('tipCalculator');;
+Route::get('/fallout-randomizer', 'App\Http\Controllers\falloutRandomizer\FalloutRandomizerController@index')->name('fallout.Randomizer');
+Route::get('/tip-calc', 'App\Http\Controllers\tipCalc\tipCalcController@index')->name('tip.Calculator');;
 
