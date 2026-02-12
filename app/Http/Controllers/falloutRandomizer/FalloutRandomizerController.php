ced3aa9d61d52feeb4a2eacc665f51debe4cc613
Brandon Fields <superboomda@gmail.com>
2026-02-11 20:53:30 -0500

    Updated FalloutRandomizerController to use the base controller

--- app/Http/Controllers/falloutRandomizer/FalloutRandomizerController.php
+++ app/Http/Controllers/falloutRandomizer/FalloutRandomizerController.php
@@ -2,6 +2,8 @@
  
 namespace App\Http\Controllers\falloutRandomizer;
 
+use App\Http\Controllers\Controller;
+
 class FalloutRandomizerController extends Controller
 {
     public function index(Request $request) {
