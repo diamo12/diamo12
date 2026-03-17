cbf43a0f44b78e693f838fb71bea5cce306d76a7
Brandon Fields <superboomda@gmail.com>
2026-03-16 22:40:33 -0400

    Fixing bug in js

--- resources/views/guestbook/index.blade.php
+++ resources/views/guestbook/index.blade.php
@@ -17,9 +17,6 @@
         //for each guestbook log, update template and append to guestbook-logs timeline component in content.
     });
 
-    //set template
-    let template = "<div class=\"rvt-timeline__item\"><div class=\"rvt-timeline__marker\" aria-hidden=\"true\"></div><div class=\"rvt-timeline__content\"><h2 class=\"rvt-timeline__heading\">{name}</h2><span class=\"rvt-timeline__date\">{website-url}</span><p>{message}</p></div></div>";
-
     if(results!=null) {
         
         
