e7d62ad5caf4070018daeddb9f91be81521be7c7
Brandon Fields <superboomda@gmail.com>
2026-03-16 22:40:01 -0400

    Still learning how to manipulate the data

--- resources/views/guestbook/index.blade.php
+++ resources/views/guestbook/index.blade.php
@@ -8,18 +8,20 @@
 @push('js')
 <script type="text/javascript">
     var timeline = document.getElementById('guestbook-logs');
-    let results = "";
+    //set template
+    let template = "<div class=\"rvt-timeline__item\"><div class=\"rvt-timeline__marker\" aria-hidden=\"true\"></div><div class=\"rvt-timeline__content\"><h2 class=\"rvt-timeline__heading\">{name}</h2><span class=\"rvt-timeline__date\">{website-url}</span><p>{message}</p></div></div>";
     //get data
     fetchGuestbookData().then(value => {
         console.log(value);
-        results = value;
+        console.log(value.data);
+        //for each guestbook log, update template and append to guestbook-logs timeline component in content.
     });
 
     //set template
     let template = "<div class=\"rvt-timeline__item\"><div class=\"rvt-timeline__marker\" aria-hidden=\"true\"></div><div class=\"rvt-timeline__content\"><h2 class=\"rvt-timeline__heading\">{name}</h2><span class=\"rvt-timeline__date\">{website-url}</span><p>{message}</p></div></div>";
 
     if(results!=null) {
-        //for each guestbook log, update template and append to guestbook-logs timeline component in content.
+        
         
         console.log(results);
         
